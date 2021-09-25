3341d8caf3e99ea30bd20b4217e9fe885e884b30
Darshan H K <darshandarru1@gmail.com>
2021-09-25 14:33:30 +0530

    Project

---
+++ Darshan H K_46131549/src/Question1/TaxCalculatorMain.java
@@ -0,0 +1,12 @@
+package Question1;
+
+public class TaxCalculatorMain {
+
+	public static void main(String[] args)
+	{
+		
+		TaxCalculator t = new TaxCalculator();
+		System.out.println("Taxable Amount is " +t.taxableAmount(40000));
+	}
+
+}
